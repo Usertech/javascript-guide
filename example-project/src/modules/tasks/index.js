6e456e593b2e12af9b0e7650ac8fdfb3d985028e
uhrik.tom@gmail.com <uhrik.tom@gmail.com>
2017-12-06 16:15:45 +0100

    polished modules

--- example-project/src/modules/tasks/index.js
+++ example-project/src/modules/tasks/index.js
@@ -1,14 +1,4 @@
 import createModule from 'client-core/lib/utils/createModule';
-import { takeEvery, put } from 'redux-saga/effects';
 import reducer from './reducer';
-import tasksData from '../../data/tasks.json';
 
-function* requestTasks() {
-	yield put({ type: 'RECEIVE_TASKS', payload: { data: tasksData } });
-}
-
-function* watch() {
-	yield takeEvery('REQUEST_TASKS', requestTasks);
-}
-
-export default createModule('tasks', reducer, [watch]);
+export default createModule('tasks', reducer);
